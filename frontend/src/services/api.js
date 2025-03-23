adb87d10e8681d0aa519ca24f235c261a026ced4
Baabumoshaay <mayankgautam1404@gmail.com>
2025-03-23 10:18:35 +0530

    CI/CD Pipeline Deployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://54.91.180.154:5000/api",
 });
 
 // Add a request interceptor to include the JWT token in headers
